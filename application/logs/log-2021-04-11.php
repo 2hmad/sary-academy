ab674bc5fc9c97213b5fd93e6a2a6d56a10d3b4d
coduxcompany <eng.ahmedmohamed.2002@gmail.com>
2021-04-11 02:56:57 +0200

    .

---
+++ application/logs/log-2021-04-11.php
@@ -0,0 +1,41 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-04-11 02:24:58 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'aqtardes_saryadm'@'localhost' (using password: YES) C:\xampp\htdocs\sary-academy\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-04-11 02:24:58 --> Unable to connect to the database
+ERROR - 2021-04-11 02:24:58 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'aqtardes_saryadm'@'localhost' (using password: YES) C:\xampp\htdocs\sary-academy\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-04-11 02:24:58 --> Severity: Warning --> ini_set(): A session is active. You cannot change the session module's ini settings at this time C:\xampp\htdocs\sary-academy\system\libraries\Session\Session_driver.php 205
+ERROR - 2021-04-11 02:24:58 --> Severity: Warning --> session_start(): Failed to initialize storage module: user (path: C:\xampp\tmp) C:\xampp\htdocs\sary-academy\system\libraries\Session\Session.php 143
+ERROR - 2021-04-11 02:24:58 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'aqtardes_saryadm'@'localhost' (using password: YES) C:\xampp\htdocs\sary-academy\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-04-11 02:24:58 --> Unable to connect to the database
+ERROR - 2021-04-11 02:24:58 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'aqtardes_saryadm'@'localhost' (using password: YES) C:\xampp\htdocs\sary-academy\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-04-11 02:24:58 --> Unable to connect to the database
+ERROR - 2021-04-11 02:24:58 --> Query error: Access denied for user 'aqtardes_saryadm'@'localhost' (using password: YES) - Invalid query: SELECT *
+FROM `settings`
+WHERE `id` = 1
+ERROR - 2021-04-11 02:24:58 --> Severity: error --> Exception: Call to a member function row_array() on bool C:\xampp\htdocs\sary-academy\application\helpers\common_helper.php 15
+ERROR - 2021-04-11 02:25:03 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'aqtardes_saryadm'@'localhost' (using password: YES) C:\xampp\htdocs\sary-academy\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-04-11 02:25:03 --> Unable to connect to the database
+ERROR - 2021-04-11 02:25:03 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'aqtardes_saryadm'@'localhost' (using password: YES) C:\xampp\htdocs\sary-academy\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-04-11 02:25:03 --> Severity: Warning --> ini_set(): A session is active. You cannot change the session module's ini settings at this time C:\xampp\htdocs\sary-academy\system\libraries\Session\Session_driver.php 205
+ERROR - 2021-04-11 02:25:03 --> Severity: Warning --> session_start(): Failed to initialize storage module: user (path: C:\xampp\tmp) C:\xampp\htdocs\sary-academy\system\libraries\Session\Session.php 143
+ERROR - 2021-04-11 02:25:03 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'aqtardes_saryadm'@'localhost' (using password: YES) C:\xampp\htdocs\sary-academy\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-04-11 02:25:03 --> Unable to connect to the database
+ERROR - 2021-04-11 02:25:03 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'aqtardes_saryadm'@'localhost' (using password: YES) C:\xampp\htdocs\sary-academy\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-04-11 02:25:03 --> Unable to connect to the database
+ERROR - 2021-04-11 02:25:03 --> Query error: Access denied for user 'aqtardes_saryadm'@'localhost' (using password: YES) - Invalid query: SELECT *
+FROM `settings`
+WHERE `id` = 1
+ERROR - 2021-04-11 02:25:03 --> Severity: error --> Exception: Call to a member function row_array() on bool C:\xampp\htdocs\sary-academy\application\helpers\common_helper.php 15
+ERROR - 2021-04-11 02:45:43 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'aqtardes_saryadm'@'localhost' (using password: YES) C:\xampp\htdocs\sary-academy\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-04-11 02:45:43 --> Unable to connect to the database
+ERROR - 2021-04-11 02:45:43 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'aqtardes_saryadm'@'localhost' (using password: YES) C:\xampp\htdocs\sary-academy\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-04-11 02:45:43 --> Severity: Warning --> ini_set(): A session is active. You cannot change the session module's ini settings at this time C:\xampp\htdocs\sary-academy\system\libraries\Session\Session_driver.php 205
+ERROR - 2021-04-11 02:45:43 --> Severity: Warning --> session_start(): Failed to initialize storage module: user (path: C:\xampp\tmp) C:\xampp\htdocs\sary-academy\system\libraries\Session\Session.php 143
+ERROR - 2021-04-11 02:45:43 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'aqtardes_saryadm'@'localhost' (using password: YES) C:\xampp\htdocs\sary-academy\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-04-11 02:45:43 --> Unable to connect to the database
+ERROR - 2021-04-11 02:45:43 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'aqtardes_saryadm'@'localhost' (using password: YES) C:\xampp\htdocs\sary-academy\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-04-11 02:45:43 --> Unable to connect to the database
+ERROR - 2021-04-11 02:45:43 --> Query error: Access denied for user 'aqtardes_saryadm'@'localhost' (using password: YES) - Invalid query: SELECT *
+FROM `settings`
+WHERE `id` = 1
+ERROR - 2021-04-11 02:45:43 --> Severity: error --> Exception: Call to a member function row_array() on bool C:\xampp\htdocs\sary-academy\application\helpers\common_helper.php 15
