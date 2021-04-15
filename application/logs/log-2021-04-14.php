14291e5c2aace5d26d909da4cb8af7d3d3e104bc
coduxcompany <eng.ahmedmohamed.2002@gmail.com>
2021-04-15 14:25:13 +0200

    .

---
+++ application/logs/log-2021-04-14.php
@@ -0,0 +1,32 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-04-14 14:38:27 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'aqtardes_saryadm'@'localhost' (using password: YES) C:\xampp\htdocs\sary-academy\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-04-14 14:38:27 --> Unable to connect to the database
+ERROR - 2021-04-14 14:38:27 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'aqtardes_saryadm'@'localhost' (using password: YES) C:\xampp\htdocs\sary-academy\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-04-14 14:38:27 --> Severity: Warning --> ini_set(): A session is active. You cannot change the session module's ini settings at this time C:\xampp\htdocs\sary-academy\system\libraries\Session\Session_driver.php 205
+ERROR - 2021-04-14 14:38:27 --> Severity: Warning --> session_start(): Failed to initialize storage module: user (path: C:\xampp\tmp) C:\xampp\htdocs\sary-academy\system\libraries\Session\Session.php 143
+ERROR - 2021-04-14 14:38:27 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'aqtardes_saryadm'@'localhost' (using password: YES) C:\xampp\htdocs\sary-academy\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-04-14 14:38:27 --> Unable to connect to the database
+ERROR - 2021-04-14 14:38:27 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'aqtardes_saryadm'@'localhost' (using password: YES) C:\xampp\htdocs\sary-academy\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-04-14 14:38:27 --> Unable to connect to the database
+ERROR - 2021-04-14 14:38:27 --> Query error: Access denied for user 'aqtardes_saryadm'@'localhost' (using password: YES) - Invalid query: SELECT *
+FROM `settings`
+WHERE `id` = 1
+ERROR - 2021-04-14 14:38:27 --> Severity: error --> Exception: Call to a member function row_array() on bool C:\xampp\htdocs\sary-academy\application\helpers\common_helper.php 15
+ERROR - 2021-04-14 16:28:26 --> 404 Page Not Found: Superadmin/activities
+ERROR - 2021-04-14 16:29:20 --> 404 Page Not Found: Superadmin/activities
+ERROR - 2021-04-14 16:31:58 --> 404 Page Not Found: Superadmin/activities
+ERROR - 2021-04-14 16:32:00 --> 404 Page Not Found: Superadmin/activities
+ERROR - 2021-04-14 16:32:23 --> 404 Page Not Found: Superadmin/activities
+ERROR - 2021-04-14 16:33:17 --> 404 Page Not Found: Superadmin/activities
+ERROR - 2021-04-14 16:33:17 --> 404 Page Not Found: Superadmin/activities
+ERROR - 2021-04-14 16:34:00 --> 404 Page Not Found: Superadmin/activities
+ERROR - 2021-04-14 16:34:32 --> 404 Page Not Found: Superadmin/activities
+ERROR - 2021-04-14 16:34:53 --> 404 Page Not Found: Superadmin/activities
+ERROR - 2021-04-14 16:35:49 --> 404 Page Not Found: Superadmin/activities
+ERROR - 2021-04-14 17:01:48 --> 404 Page Not Found: Superadmin/activities
+ERROR - 2021-04-14 17:30:31 --> 404 Page Not Found: Superadmin/activities
+ERROR - 2021-04-14 17:30:50 --> 404 Page Not Found: Superadmin/activities
+ERROR - 2021-04-14 18:07:33 --> 404 Page Not Found: Superadmin/activities
+ERROR - 2021-04-14 18:08:37 --> 404 Page Not Found: Superadmin/activities
+ERROR - 2021-04-14 18:09:20 --> 404 Page Not Found: Superadmin/activities
